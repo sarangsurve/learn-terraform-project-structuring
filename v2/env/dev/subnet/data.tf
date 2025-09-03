52b2778f62ac38a4ca361571e60f602a96195fd5
sarangsurve <sarangsurve126@gmail.com>
2025-09-03 11:39:42 +0530

    cd v2\env\dev\subnet && terraform init && terraform apply --auto-approve

---
+++ v2/env/dev/subnet/data.tf
@@ -0,0 +1,10 @@
+data "terraform_remote_state" "vpc" {
+    backend = "local"
+    config = {
+        path = "../vpc/terraform.tfstate"
+    }
+}
+
+output "test" {
+  value = data.terraform_remote_state.vpc
+}
---
+++ v2/modules/subnet/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cidr_block" {
+  default = "10.0.0.0/19"
+}
+
+variable "env" {
+  default = "prod"
+}
+
+variable "vpc_id" {}
