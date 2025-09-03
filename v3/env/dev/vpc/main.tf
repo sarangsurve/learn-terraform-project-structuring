e2a9fb35f1cf73682405c9cac017527239d8b991
sarangsurve <sarangsurve126@gmail.com>
2025-09-03 12:58:56 +0530

    cd v3\env\dev\vpc && terraform init && terraform apply --auto-approve && sleep 60 && terraform destroy --auto-approve

--- v3/env/dev/vpc/main.tf
+++ v3/env/dev/vpc/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git@github.com:sarangsurve/terraform-project-structuring-v3-vpc-module.git?ref=0.1.0"
+  source = "git@github.com:sarangsurve/terraform-project-structuring-v3-vpc-module.git?ref=0.1.1"
 
   env = "dev"
   cidr_block = "10.0.0.0/16"
