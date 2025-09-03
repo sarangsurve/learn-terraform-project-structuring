f83241d4239b598193ac8097d9aa7184b47c7693
sarangsurve <sarangsurve126@gmail.com>
2025-09-03 09:48:58 +0530

    terraform apply

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -8,3 +8,13 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-main"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+
+  tags = {
+    "Name" = "dev-private"
+  }
+  
+}
